67c66c043407df3e82e385029b1ee10e0c9a0ed8
Satya Prakash <thatissatya@gmail.com>
2024-03-02 23:12:09 +0530

    local file imports

--- Day-1/notes-app/app.js
+++ Day-1/notes-app/app.js
@@ -1,11 +1,17 @@
 // loading file-system module, part of nodeJs not javascript
 const fs = require('fs')
+const utils = require('./utils')
 
-// writing data to a file synchronously. syntax : fs.writeFileSync(file_name, file_content to be written)
-fs.writeFileSync("note.txt", "The quick brown fox jumps over the lazy dog.\n")
+const getNotes = require('./notes')
+
+console.log(getNotes())
+
+// // writing data to a file synchronously. syntax : fs.writeFileSync(file_name, file_content to be written)
+// fs.writeFileSync("note.txt", "The quick brown fox jumps over the lazy dog.\n")
 
 
-// Challenge : Append a Message to note.txt
+// // Challenge : Append a Message to note.txt
 
-fs.appendFileSync("note.txt", "This sentence contains all the english alphabet.")
+// fs.appendFileSync("note.txt", "This sentence contains all the english alphabet.")
 
+// console.log(utils.name)
---
+++ Day-1/notes-app/utils.js
@@ -0,0 +1,5 @@
+console.log("this is utility file.")
+
+const name = "Bob"
+
+module.exports = {name:name}
