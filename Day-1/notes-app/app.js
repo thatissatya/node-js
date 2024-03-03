82dc76fdd9e87a9f51542951097a800868a50b6a
Satya Prakash <thatissatya@gmail.com>
2024-03-03 13:32:38 +0530

    changes

--- Day-1/notes-app/app.js
+++ Day-1/notes-app/app.js
@@ -1,4 +1,11 @@
 // loading module, part of nodeJs not javascript
 const getNotes = require('./notes')
+const validator = require('validator')
+const chalk = require('chalk')
+// import chalk from 'chalk'
 
 console.log(getNotes())
+
+console.log(validator.isEmail("satya@gmail.com"));
+console.log(chalk.blue.italic(validator.isURL("gmail.com")))
+console.log(chalk.red.bold("success!"))
